8e4cce67a5412e837fc21b24836cac0bfe55d94d
FAUCHET Malo <malofauchet@gmail.com>
2023-10-22 20:19:19 +0200

    added check_num func

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess. ");
 
+    let number: i8 = 54;
+
     let mut guess:  String= String::new();
 
     io::stdin()
@@ -11,4 +13,15 @@ fn main() {
         .expect("Failed to read line");
 
     println!("\nYou guessed: {guess}");
+    check_num(guess, number);
+}
+
+fn check_num(guess: i8, number: i8) {
+    if guess == number {
+        println!("You win!");
+    } else if guess < number {
+        println!("Higher");
+    } else {
+        println!("Lower");
+    }
 }
